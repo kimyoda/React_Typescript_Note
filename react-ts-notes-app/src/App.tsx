36d2cb976b0a672d5ae8926552f68567a6819105
kimyoda <rladygks1210@naver.com>
2024-11-02 02:51:42 +0900

    [feat] first setting

---
+++ react-ts-notes-app/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1 className="title has-text-centered">노트앱</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
