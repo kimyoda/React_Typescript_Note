21d79dc4204167ef982ea76a36748632061e1cd7
kimyoda <rladygks1210@naver.com>
2024-11-02 10:19:27 +0900

    [feat] Note components add

--- react-ts-notes-app/src/App.tsx
+++ react-ts-notes-app/src/App.tsx
@@ -1,8 +1,39 @@
+import { Note } from "components";
+import { useState } from "react";
+import { NoteInterface } from "types";
+
 function App() {
+  const [notes, setNotes] = useState<NoteInterface[]>([]);
+
+  const [noteToDelete, setNoteToDelete] = useState<NoteInterface | null>(null);
+  const [noteToEdit, setNoteToEdit] = useState<NoteInterface | null>(null);
+
+  const onNoteDeleteClick = (note: NoteInterface) => {
+    setNoteToDelete(note);
+  };
+  const onNoteEditClick = (note: NoteInterface) => {
+    setNoteToEdit(note);
+  };
+
   return (
     <div className="container">
       <div className="py-4">
         <h1 className="title has-text-centered">노트앱</h1>
+      </div>
+      <div className="tile is-flex-wrap-wrap">
+        {notes.map((note) => {
+          return (
+            <Note
+              key={note.id}
+              message={note.message}
+              onDelete={() => onNoteDeleteClick(note)}
+              onEdit={() => onNoteEditClick(note)}
+            />
+          );
+        })}
+        <div className="tile is-3 p-2">
+          <div className="box is-fullwidth"></div>
+        </div>
       </div>
     </div>
   );
